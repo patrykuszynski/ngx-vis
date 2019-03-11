5d4130c8e143511092a8dba14389f505ea82289a
patrykuszynski <patryk.uszynski@coinfirm.io>
2019-03-11 17:17:38 +0100

    NgZone added

--- components/network/vis-network.directive.ts
+++ components/network/vis-network.directive.ts
@@ -1,19 +1,20 @@
 import {
-  Directive,
-  ElementRef,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  Output,
-  SimpleChange } from '@angular/core';
+    Directive,
+    ElementRef,
+    EventEmitter,
+    Input,
+    NgZone,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    Output,
+    SimpleChange} from '@angular/core';
 
 import { VisNetworkService } from './vis-network.service';
 
 import {
-  VisNetworkData,
-  VisNetworkOptions,
+    VisNetworkData,
+    VisNetworkOptions,
 } from './index';
 
 /**
@@ -26,129 +27,132 @@ import {
  * @implements {OnChanges}
  */
 @Directive({
-  selector: '[visNetwork]',
+    selector: '[visNetwork]',
 })
 export class VisNetworkDirective implements OnInit, OnDestroy, OnChanges {
 
-  /**
-   * The name or identifier of the network (must be unique in your application).
-   * This property is used once on init and must not be changed.
-   *
-   * @type {string}
-   * @memberOf VisNetworkDirective
-   */
-  @Input('visNetwork')
-  public visNetwork: string;
+    /**
+     * The name or identifier of the network (must be unique in your application).
+     * This property is used once on init and must not be changed.
+     *
+     * @type {string}
+     * @memberOf VisNetworkDirective
+     */
+    @Input('visNetwork')
+    public visNetwork: string;
 
-  /**
-   * The data that will be used to create the network.
-   * Changes to the nodes or edges property won't be detected but
-   * changes to the reference of this object.
-   * Changes lead to a call to setData of this network instance.
-   *
-   * @type {VisNetworkData}
-   * @memberOf VisNetworkDirective
-   */
-  @Input()
-  public visNetworkData: VisNetworkData;
+    /**
+     * The data that will be used to create the network.
+     * Changes to the nodes or edges property won't be detected but
+     * changes to the reference of this object.
+     * Changes lead to a call to setData of this network instance.
+     *
+     * @type {VisNetworkData}
+     * @memberOf VisNetworkDirective
+     */
+    @Input()
+    public visNetworkData: VisNetworkData;
 
-  /**
-   * The options that will be used with this network instance.
-   * Only reference changes to the whole options object will be detected
-   * but not changes to properties.
-   * Changes lead to a call to setOptions of the network instance.
-   *
-   * @type {VisNetworkOptions}
-   * @memberOf VisNetworkDirective
-   */
-  @Input()
-  public visNetworkOptions: VisNetworkOptions;
+    /**
+     * The options that will be used with this network instance.
+     * Only reference changes to the whole options object will be detected
+     * but not changes to properties.
+     * Changes lead to a call to setOptions of the network instance.
+     *
+     * @type {VisNetworkOptions}
+     * @memberOf VisNetworkDirective
+     */
+    @Input()
+    public visNetworkOptions: VisNetworkOptions;
 
-  /**
-   * This event will be raised when the network is initialized.
-   * At this point of time the network is successfully registered
-   * with the VisNetworkService and you can register to events.
-   * The event data is the name of the network as a string.
-   *
-   * @type {EventEmitter<any>}
-   * @memberOf VisNetworkDirective
-   */
-  @Output()
-  public initialized: EventEmitter<any> = new EventEmitter<any>();
+    /**
+     * This event will be raised when the network is initialized.
+     * At this point of time the network is successfully registered
+     * with the VisNetworkService and you can register to events.
+     * The event data is the name of the network as a string.
+     *
+     * @type {EventEmitter<any>}
+     * @memberOf VisNetworkDirective
+     */
+    @Output()
+    public initialized: EventEmitter<any> = new EventEmitter<any>();
 
-  private visNetworkContainer: any;
-  private isInitialized: boolean = false;
+    private visNetworkContainer: any;
+    private isInitialized: boolean = false;
 
-  /**
-   * Creates an instance of VisNetworkDirective.
-   *
-   * @param {ElementRef} elementRef The HTML element reference.
-   * @param {VisNetworkService} visNetworkService The VisNetworkService.
-   *
-   * @memberOf VisNetworkDirective
-   */
-  public constructor(private elementRef: ElementRef, private visNetworkService: VisNetworkService) {
-    this.visNetworkContainer = elementRef.nativeElement;
-  }
-
-  /**
-   * Create the network when at least visNetwork and visNetworkData
-   * are defined.
-   *
-   * @memberOf VisNetworkDirective
-   */
-  public ngOnInit(): void {
-    if (!this.isInitialized && this.visNetwork && this.visNetworkData) {
-      this.createNetwork();
-    }
-  }
-
-  /**
-   * Update the network data or options on reference changes to
-   * the visNetworkData or visNetworkOptions properties.
-   *
-   * @param {{[propName: string]: SimpleChange}} changes
-   *
-   * @memberOf VisNetworkDirective
-   */
-  public ngOnChanges(changes: {[propName: string]: SimpleChange}): void {
-
-    if (!this.isInitialized && this.visNetwork && this.visNetworkData) {
-      this.createNetwork();
+    /**
+     * Creates an instance of VisNetworkDirective.
+     *
+     * @param {ElementRef} elementRef The HTML element reference.
+     * @param {VisNetworkService} visNetworkService The VisNetworkService.
+     *
+     * @memberOf VisNetworkDirective
+     */
+    public constructor(private elementRef: ElementRef, private visNetworkService: VisNetworkService, private ngZone: NgZone) {
+        this.visNetworkContainer = elementRef.nativeElement;
     }
 
-    for (const propertyName in changes) {
-      if (changes.hasOwnProperty(propertyName)) {
-        const change = changes[propertyName];
-        if (!change.isFirstChange()) {
-          if (propertyName === 'visNetworkData') {
-            this.visNetworkService.setData(this.visNetwork, changes[propertyName].currentValue);
-          }
-          if (propertyName === 'visNetworkOptions') {
-            this.visNetworkService.setOptions(this.visNetwork, changes[propertyName].currentValue);
-          }
+    /**
+     * Create the network when at least visNetwork and visNetworkData
+     * are defined.
+     *
+     * @memberOf VisNetworkDirective
+     */
+    public ngOnInit(): void {
+        if (!this.isInitialized && this.visNetwork && this.visNetworkData) {
+            this.createNetwork();
         }
-      }
     }
-  }
 
-  /**
-   * Calls the destroy function for this network instance.
-   *
-   * @memberOf VisNetworkDirective
-   */
-  public ngOnDestroy(): void {
-    this.isInitialized = false;
-    this.visNetworkService.destroy(this.visNetwork);
-  }
+    /**
+     * Update the network data or options on reference changes to
+     * the visNetworkData or visNetworkOptions properties.
+     *
+     * @param {{[propName: string]: SimpleChange}} changes
+     *
+     * @memberOf VisNetworkDirective
+     */
+    public ngOnChanges(changes: {[propName: string]: SimpleChange}): void {
+        if (!this.isInitialized && this.visNetwork && this.visNetworkData) {
+            this.createNetwork();
+        }
 
-  private createNetwork(): void {
-    this.visNetworkService.create(
-      this.visNetwork,
-      this.visNetworkContainer,
-      this.visNetworkData,
-      this.visNetworkOptions);
-    this.isInitialized = true;
-    this.initialized.emit(this.visNetwork);
-  }
+        this.ngZone.runOutsideAngular(() => {
+            for (const propertyName in changes) {
+                if (changes.hasOwnProperty(propertyName)) {
+                    const change = changes[propertyName];
+                    if (!change.isFirstChange()) {
+                        if (propertyName === 'visNetworkData') {
+                            this.visNetworkService.setData(this.visNetwork, changes[propertyName].currentValue);
+                        }
+                        if (propertyName === 'visNetworkOptions') {
+                            this.visNetworkService.setOptions(this.visNetwork, changes[propertyName].currentValue);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Calls the destroy function for this network instance.
+     *
+     * @memberOf VisNetworkDirective
+     */
+    public ngOnDestroy(): void {
+        this.isInitialized = false;
+        this.visNetworkService.destroy(this.visNetwork);
+    }
+
+    private createNetwork(): void {
+        this.ngZone.runOutsideAngular(() => {
+            this.visNetworkService.create(
+                this.visNetwork,
+                this.visNetworkContainer,
+                this.visNetworkData,
+                this.visNetworkOptions);
+            this.isInitialized = true;
+            this.initialized.emit(this.visNetwork);
+        });
+    }
 }
